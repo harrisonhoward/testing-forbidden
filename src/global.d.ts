b9af597de87629c93bbeb9a7683268d82a1152f9
Harrison Howard <harrison.howard00707@gmail.com>
2023-04-01 13:24:33 +1000

    Remove forbidden-node-logger add PREFIX

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,6 @@
 export interface Env {
     BOT_TOKEN: string;
+    PREFIX: string;
 }
 
 // Extend process.env to include our custom variables
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
 import { SapphireClient } from "@sapphire/framework";
 import { GatewayIntentBits } from "discord.js";
 
-import NodeLogger from "forbidden-node-logger";
-
 // Setup dotenv for our '.env' file with our token under 'BOT_TOKEN
 import dotenv from "dotenv";
 dotenv.config();
 
 // Setup our client
-const client = new SapphireClient({
+const bot = new SapphireClient({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+    defaultPrefix: process.env.PREFIX,
+    baseUserDirectory: __dirname + "/",
 });
 
-// Setup our logger
-export const Logger = new NodeLogger.Logger({ dirPath: __dirname + "/logs" });
-
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
